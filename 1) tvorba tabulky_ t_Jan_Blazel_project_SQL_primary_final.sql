d0a7e4b3dc425969e794e2f48e01e1700acba197
Jan Blažek <blazek.km@hotmail.com>
2023-12-18 18:44:54 +0100

    SQL příkaz pro vytvoření primary tabulky k SQL projektu

--- 1) tvorba tabulky_ t_Jan_Blazel_project_SQL_primary_final.sql
+++ 1) tvorba tabulky_ t_Jan_Blazel_project_SQL_primary_final.sql
@@ -21,7 +21,30 @@ JOIN czechia_payroll cpr
 JOIN czechia_payroll_industry_branch cpib 
 	ON cpr.industry_branch_code = cpib.code; 
 
+DROP TABLE IF EXISTS t_Jan_Blazel_project_SQL_primary_final;
 
+CREATE TABLE IF NOT EXISTS t_Jan_Blazel_project_SQL_primary_final AS 
+SELECT 
+	cp.category_code,
+	cp.value AS cp_value,
+	YEAR (cp.date_from) AS cp_year ,
+	cpc.code AS cpc_code,
+	cpc.name AS cpc_name,
+	cpc.price_unit,
+	cpr.payroll_year AS payroll_year,
+	cpr.value AS cpr_value, 
+	cpr.value_type_code,
+	cpr.unit_code,
+	cpr.industry_branch_code,
+	cpib.code AS cpib_code,
+	cpib.name AS cpib_name
+FROM czechia_price cp 
+JOIN czechia_price_category cpc 
+	ON cp.category_code = cpc.code 
+JOIN czechia_payroll cpr 
+	ON YEAR (cp.date_from) = cpr.payroll_year 
+JOIN czechia_payroll_industry_branch cpib 
+	ON cpr.industry_branch_code = cpib.code; 
 
 
 
