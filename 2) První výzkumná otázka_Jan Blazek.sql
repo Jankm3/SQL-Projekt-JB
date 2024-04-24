afb0f534d718c069baf5edcbd4b0c8a7d97776be
Jan Blažek <blazek.km@hotmail.com>
2024-04-24 06:20:14 +0200

    SQL dotaz pro zodpovězení druhé výzkumné otázky

--- 2) První výzkumná otázka_Jan Blazek.sql
+++ 2) První výzkumná otázka_Jan Blazek.sql
@@ -1,25 +1,41 @@
-WITH prumerne_mzdy_dle_odvetvi AS (
+WITH average_payroll AS (
 SELECT
-    cpib_name,
-    payroll_year,
-    cpr_value
+	cpib_name,
+	payroll_year,
+	avg_cpr_value
 FROM
-	t_jan_blazel_project_sql_primary_final tjbpspf 
-WHERE value_type_code = '5958'
+	t_jan_blazek_project_sql_primary_final tjb
+WHERE cpr_value_type = '5958'
 GROUP BY
-    cpib_name, payroll_year
+	cpib_name, payroll_year
 ORDER BY
-    cpib_name, payroll_year
-    )
- SELECT
-  cpib_name,
-  MIN(payroll_year) AS start_year,
-  MAX(payroll_year) AS end_year,
-  MAX(cpr_value) AS max_pay,
-  MIN(cpr_value), 
-  MAX(cpr_value) - MIN(cpr_value) AS average_salary_growth
-  FROM prumerne_mzdy_dle_odvetvi 
-  GROUP BY
-    cpib_name
+	cpib_name, payroll_year
+	)
+SELECT
+	cpib_name,
+	MIN(payroll_year) AS start_year,
+	MAX(payroll_year) AS end_year,
+	MAX(avg_cpr_value) AS max_payroll,
+	MIN(avg_cpr_value) AS  min_payroll,
+	ROUND (MAX(avg_cpr_value) - MIN(avg_cpr_value)) AS average_salary_growth
+FROM average_payroll 
+GROUP BY
+	cpib_name
 ORDER BY
-    average_salary_growth, max_pay DESC; 
+	average_salary_growth DESC; 
+   
+SELECT
+	cpib_name,
+	payroll_year,
+	ROUND (avg_cpr_value),
+	ROUND (LAG(avg_cpr_value) OVER (PARTITION BY cpib_name ORDER BY payroll_year)) AS prev_avg_cpr_value,
+	CASE
+		WHEN avg_cpr_value < LAG(avg_cpr_value) OVER (PARTITION BY cpib_name ORDER BY payroll_year) THEN 'Ano'
+		ELSE 'Ne'
+	END AS has_decreased
+FROM
+	t_jan_blazek_project_sql_primary_final
+WHERE
+	cpr_value_type = '5958'
+ORDER BY 
+	has_decreased;
--- 3) Druhá výzkumná otázka_Jan Blazek.sql
+++ 3) Druhá výzkumná otázka_Jan Blazek.sql
@@ -1,37 +1,35 @@
-
 SELECT 
 min (payroll_year),
 max (payroll_year),
 min(cp_year),
 max (cp_year)
-FROM t_jan_blazel_project_sql_primary_final tjbpspf ;
+FROM t_jan_blazek_project_sql_primary_final tjb;
 
-CREATE VIEW average_pay AS 
+WITH average_pay AS (
+	SELECT 
+		AVG (avg_cpr_value) AS avg_cpr_value,
+		payroll_year
+	FROM t_jan_blazek_project_sql_primary_final tjb
+	WHERE payroll_year IN ('2006', '2018')
+		AND cpr_value_type = '5958'
+	GROUP BY payroll_year
+),
+prices_of_selected_goods AS (
+	SELECT 
+		cpc_name,
+		cpc_code,
+		AVG (avg_cp_value) AS avg_cp_value,
+		payroll_year 
+	FROM t_jan_blazek_project_sql_primary_final tjb 
+	WHERE cpc_code IN ('114201', '111301')
+		AND payroll_year IN ('2006', '2018')
+	GROUP BY cpc_name, payroll_year
+)
 SELECT 
-	avg (cpr_value) AS average_pay,
-	payroll_year
-FROM t_jan_blazel_project_sql_primary_final tjbpspf 
-WHERE payroll_year IN ('2006', '2018')
-	AND value_type_code = '5958'
-GROUP BY payroll_year;
-	
-CREATE VIEW prices_of_selected_goods AS 	
-SELECT 
-	cpc_name,
-	cpc_code,
-	cp_value,
-	payroll_year 
-FROM t_jan_blazel_project_sql_primary_final tjbpspf 
-WHERE cpc_code IN ('114201', '111301')
-	AND payroll_year IN ('2006', '2018')
-GROUP BY cpc_name, payroll_year  ;
-
-
-SELECT 
-posg.cpc_name,
-ap.payroll_year,
-round ((average_pay / cp_value), 2) AS quantity_of_products
+	posg.cpc_name,
+	ap.payroll_year,
+	ROUND((ap.avg_cpr_value / posg.avg_cp_value), 2) AS quantity_of_products
 FROM average_pay ap 
 JOIN prices_of_selected_goods posg 
 	ON ap.payroll_year = posg.payroll_year 
-	GROUP BY ap.payroll_year, cpc_name ;
+GROUP BY ap.payroll_year, posg.cpc_name;
